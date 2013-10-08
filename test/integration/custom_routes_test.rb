0b140dac77b8568c0437ccdb048bc9a80b3bea65
Ed Holden <ed@holden2.com>
2013-10-08 18:20:35 -0500

    checking commit

---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+	test "that the /login route opens the login page"   do 
+		get '/login'
+		assert_response :success
+	end
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+	test "that the /logout route opens the logout page"   do 
+		get '/logout'
+		assert_response :redirect
+		assert_redirected_to '/'
+	end
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+	test "that the /register route opens the register page"   do 
+		get '/register'
+		assert_response :success
+	end
+end
+end
+end
